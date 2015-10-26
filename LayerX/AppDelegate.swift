662df86190f4450a1b45a7f3ca53c2461427d1c4
Michael Chen <michaelchen@kkbox.com>
2015-10-26 17:48:14 +0800

    Initial Commit

---
+++ LayerX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LayerX
+//
+//  Created by Michael Chen on 2015/10/26.
+//  Copyright © 2015年 Michael Chen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ LayerX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LayerX
+//
+//  Created by Michael Chen on 2015/10/26.
+//  Copyright © 2015年 Michael Chen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
